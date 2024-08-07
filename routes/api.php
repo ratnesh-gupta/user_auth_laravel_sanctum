b75be927e76ba88ec007d7412b80cac03ec6244d
Ratnesh Gupta <ratneshkrgupt@gmail.com>
2024-08-07 18:14:56 +0530

    Auth structure added to the code

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('register',[RegisterController::class,'register']);
+Route::post('login',[LoginController::class,'login']);
+
+
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('logout',[LogoutController::class,'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
